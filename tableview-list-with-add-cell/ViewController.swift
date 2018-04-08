12b542e900662671a7db8f414ad7b6b6e0962f0d
Magnus Kahr Jensen <201607886@post.au.dk>
2018-04-08 21:33:58 +0200

    Initial Commit

---
+++ tableview-list-with-add-cell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableview-list-with-add-cell
+//
+//  Created by Magnus Jensen on 08/04/2018.
+//  Copyright © 2018 Magnus Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
